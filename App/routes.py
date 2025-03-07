c5c7a256276bf52c3507311b76e92a1e29cae898
Isaiah Strouse <113122370+StrizzyAllen@users.noreply.github.com>
2025-03-07 09:31:53 -0600

    Update routes.py
    Added starter code to routes.py

--- App/routes.py
+++ App/routes.py
@@ -1,1 +1,5 @@
-
+from flask import Blueprint, jsonify
+main = Blueprint("main", __name__)
+@main.route("/", methods=["GET"])
+def home():
+  return jsonify({"message: "Job Scraper API is running!"}) 
